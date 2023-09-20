385764961db787bba04c8d1c8370bdd6f56ca844
Amisha-Singh-2002 <amishasingh2302@gmail.com>
2023-09-20 13:47:08 +0530

    arnab prof pic update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -144,7 +144,7 @@ function About() {
           />
            <Members
             name="Arnab Chakraborty"
-            photo="./arnab.jpg"
+            photo="./arnab2.jpg"
             instagram="https://instagram.com/arnab_chakraborty_black_jack?igshid=YTQwZjQ0NmI0OA==/"
             facebook="https://www.facebook.com/arnab.chakraborty.378199?mibextid=ZbWKwL"
             linkedin="https://www.linkedin.com/in/arnab-chakraborty-737492230/"
